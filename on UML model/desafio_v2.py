23702b6db0aa9d0351b43536d9e81adb17060fee
luizquevedo <git.luizquevedo@gmail.com>
2025-06-05 01:01:44 -0300

    Antes de fazer duas branch (1. feature, implementar Historico; 2. feature, implementar saque e deposito)

--- on UML model/desafio_v2.py
+++ on UML model/desafio_v2.py
@@ -10,7 +10,9 @@ class Transacao(ABC):
     def registrar(self):
         pass
 
-# TODO: Tudo precisa de uma conta para existir. Faz uma aí, mano.
+# TODO: branch 1 -> conta -> deposito -> saque
+# TODO: branch 2 -> conta -> historico (registrar qqr coisa ae mano)
+
 
 #------
 
@@ -18,13 +20,14 @@ class Deposito(Transacao):
     # PREMISSA: Valores de depósito são sempre positivos.
     def __init__(self, valor):
         self.valor = valor
-    
-    def registrar(self, conta):
-        # from github repo, pruned
-        sucesso_transacao = conta.depositar(self.valor)
+        pass
 
-        if sucesso_transacao:
-            conta.adicionar_transacao(self)
+    def registrar(self):
+        # pegar o ponteiro self.historico:Historico. 
+        # extrair as transações ou a estrutura de dados
+        # append ou update
+        # fazer o set da estrutura de dados no objeto historico
+        pass
 
 
 class Saque():
@@ -39,19 +42,28 @@ class Saque():
             self.saldo -= valor
             return valor
 
-
+    def registrar(self):
+        # vide Deposito.registrar()
+        pass
 
         
 
 class Historico():
-    # Toda conta tem o seu historico.
-    def __init__(self, historico:dict={}):
-        self.historico = historico
+    # Toda conta tem o seu historico. 'self.historico = Historico()
 
-    def adicionar_transacao(self, datetime, operacao, valor):
-        # Talvez no futuro registrar() conecte uma DB?
-        transacao = {datetime: (operacao, valor)}  # dict with tuples
-        self.historico.update(transacao)
+    def __init__(self): 
+        # Talvez no futuro conecte uma DB?
+        # Ou só leia de um arquivo?
+        pass
+
+    def adicionar_transacao(self, operacao, valor):
+        datetime = datetime.now.strftime("%Y-%m-%d %H:%M:%S") 
+        transacao = {datetime: {
+            'tipo': operacao, 
+            'valor': valor,
+            }}  # dict with dicts
+        # E se duas operacoes ocorrerem ao mesmo tempo??? see note 004
+        #self.historico.update(transacao)
 
     def __str__(self):
         if len(self.historico) < 1:
@@ -66,19 +78,27 @@ class Cliente():
     def __init__(self, 
                  responsavel:object, 
                  endereco:str, 
-                 contas:list=None):
+                 contas=None):
         # 'responsavel' recebe um objeto PessoaFisica
         # 'contas' fica com uma lista de ints (ex: [0001,0002,2238])
         self.responsavel = responsavel
         self.endereco = endereco
-        self.contas = [] if contas is None else contas 
+
+        #self.contas = [] if contas is None else contas 
+        # Quem verifica se 'cliente.contas is None' é na verdade Conta.nova_conta()
+        self.contas = contas
+
         # success msg
         print('Cliente criado com sucesso!', self.__str__())
 
     def realizar_transacao(self, conta, transacao):
+        # ...pegar a conta certa
+        # ...realizar a transacao nela daí
         pass
 
     def adicionar_conta(self, conta):
+        # ...pegar a conta atual (eu acho) 
+        # ...recolher seu ID e juntar em cliente.contas
         pass
 
     def __str__(self):
@@ -92,11 +112,7 @@ class PessoaFisica():
         self.nome = nome
         self.data_nascimento = data_nascimento
         # success msg
-<<<<<<< HEAD
-        print('Sucesso!', self.__str__())
-=======
         print('\n\nSucesso!\n' + self.__str__())
->>>>>>> develop
 
     def __str__(self):
         return "(Pessoa Fisica)\n" + "\n".join([f"> {k.upper()}: {v}" for k,v in self.__dict__.items()]) + "\n"   
@@ -109,15 +125,15 @@ class Conta():
                  saldo:float=0, 
                  numero:int=None, 
                  agencia:str='0001', 
-                 historico:object=None):
+                 historico:object=None):  # possivel ataque: injetar um historico fake
         self.saldo = saldo
         self.numero = numero 
         self.agencia = agencia
         self.cliente = cliente
         self.historico = Historico()
 
-        #--por padrao, todas as contas sao conta corrente--
-        ContaCorrente.__init__(self, limite=0, limite_saques=3)
+        #--por padrao, começa tipo 'conta', podendo virar corrente--
+        pass
 
         #--fazendo raise em caso de falta de cliente --
         if self.cliente is None:
@@ -132,12 +148,18 @@ class Conta():
         return self.saldo
 
     def nova_conta(self, cliente, numero:int):
+        # checar se o cliente.contas ja possui alguma conta
+        # caso negativo: acessar DB, puxar o proximo numero, instanciar uma nova Conta, e aí se der certo adicionar ID em cliente.contas
+        # Quem adiciona em cliente.contas na verdade é Cliente.adicionar_conta()
         pass
 
     def sacar(self, valor:float) -> bool:
         pass
 
     def depositar(self, valor:float) -> bool:
+        # checar se valor < 0
+        # caso negativo chamar Deposito.depositar()
+        # see note 003. Quem vai subir pro historico na verdade é Deposito.registrar() com base no ponteiro self.historico:Historico. Repare! self.historico não é uma estrutura de dados, é um ponteiro pra um objeto que a contém.
         pass
     
     def __str__(self):
@@ -145,46 +167,35 @@ class Conta():
 
 
 
-class ContaCorrente():
+class ContaCorrente(Conta):
     def __init__(self, limite: float, limite_saques: int):
+        super().__init__(self, cliente)
         self.limite = limite
         self.limite_saques = limite_saques
+
+    
 
 
 
 #---Rodando---
-<<<<<<< HEAD
-
-## Primeiro uma pessoa entra no banco e solicita... ser cliente.
-## Dai sendo cliente, pode solicitar abrir uma conta.
-
-
-# setting a person
-luiz = PessoaFisica(12345678901, 'luizinho delas', 20010101)
-
-
-# making a person into a client
-cliente = Cliente(luiz, 'Rua dos Bobos, 0')
-
-
-# opening an account when there is none under their name
-cuenta = Conta(cliente, 0, 0, 0) 
-
-=======
 ##--Exploração inicial
 ## Primeiro uma pessoa entra no banco e solicita... ser cliente. Daí, sendo cliente, pode solicitar abrir uma conta.
 
+    
+def conta_demo():    
+    # instanciando uma pessoa
+    luiz = PessoaFisica(12345678901, 'luizinho delas', 20010101)
+    
+    
+    # 'clientizando' uma pessoa 
+    cliente = Cliente(luiz, 'Rua dos Bobos, 0')
+    
+    
+    # abrindo a primeira conta do cliente
+    conta = Conta(cliente, 0, 0, 0) 
+    
+    print('Conta demo instanciada.')
 
-# instanciando uma pessoa
-luiz = PessoaFisica(12345678901, 'luizinho delas', 20010101)
-
-
-# 'clientizando' uma pessoa 
-cliente = Cliente(luiz, 'Rua dos Bobos, 0')
-
-
-# abrindo a primeira conta do cliente
-conta = Conta(cliente, 0, 0, 0) 
 
 
 ##--Simulação de autoatendimento--
@@ -215,8 +226,20 @@ while resp := input('Abrir uma conta? [y/n]\n(aperte n e Enter para cancelar)\n>
         raise NotImplementedError('Erro. Robô não conseguiu criar uma instância de pessoa física.')
     
 
-    # 2.
-    print('NotImplemented')
+    # 2. clientizando
+    endereco = input('Endereço:\n>>> ')
+    cliente = Cliente(pessoa, endereco)
 
-print('O Banco agradece sua visita. Até a próxima!')
->>>>>>> develop
+    # 3. abrindo conta
+    conta = Conta(cliente, numero='00000001-9')
+
+    # 4. mostrando
+    # Feito via *.__str__
+    break
+
+
+    
+if not resp:
+    conta_demo()
+else:
+    print('O Banco agradece sua visita. Até a próxima!')
--- on UML model/notes.md
+++ on UML model/notes.md
@@ -2,3 +2,10 @@
 --- 
 ### note 01
 Esta é uma continuação do Desafio Bancário, do bootcamp de Python da Digital Innovation One. Nesta parte, sugere-se instanciar objetos e classes, de acordo com um diagrama UML - sem criar novos módulos (talvez arquivos me ajudem).
+
+### note 003
+Quem vai subir pro historico na verdade é Deposito.registrar() com base no ponteiro self.historico:Historico. 
+Repare! self.historico não é uma estrutura de dados, é um ponteiro pra um objeto que a contém.
+
+### note 004
+Duas operações vão acontecer no mesmo segundo??? E se? Meu filho, este programa roda na CLI. Se acontecer eu vou estar DJ Khalid: Suffering From Success. Caso no qual a gente vem e melhora a implementação. Em todo caso, *otimização prematura é a raiz de todo mal.*
